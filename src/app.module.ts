124fb3aad7cdfc7bd3e1b693ba2fa42fb3bedaf8
Gabriel da Silva Lomenha <gabriellomenha@gmail.com>
2025-07-01 22:29:24 -0300

    feat: arquivos gerados para crud de navio

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { NavioModule } from './navio/navio.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, NavioModule],
   controllers: [],
   providers: [],
 })
