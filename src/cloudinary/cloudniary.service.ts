8380ee7096d7f9cc726cb1898939d5341b48bf1d
Gabriel da Silva Lomenha <gabriellomenha@gmail.com>
2025-07-02 02:25:05 -0300

    Verificações de funções e padronização de exceções

--- src/cloudinary/cloudniary.service.ts
+++ src/cloudinary/cloudniary.service.ts
@@ -6,7 +6,7 @@ import toStream = require('buffer-to-stream');
 @Injectable()
 export class CloudinaryService {
   constructor() {
-    cloudinary.config({
+    cloudinary.config({ //Dados expostos apenas por desafio tecnico, não devem ser utilizados em produção
       cloud_name: "df7bz8she",
       api_key: "476877472667937",
       api_secret: "DFfj_H5Dv-d15nNlGTk-HNuR30s",
@@ -20,12 +20,12 @@ export class CloudinaryService {
   async upload(file: Express.Multer.File): Promise<UploadApiResponse | UploadApiErrorResponse> {
     return new Promise((resolve, reject) => {
       if (!file || !file.buffer) {
-        return reject(new BadRequestException('Arquivo não foi enviado ou não está disponível.'));
+        return reject(new HttpException('Arquivo não foi enviado ou não está disponível.', HttpStatus.BAD_REQUEST));
       }
       const upload = v2.uploader.upload_stream((error, result) => {
         if (error) {
           console.error('Erro ao fazer upload no Cloudinary:', error);
-          return reject(new BadRequestException('Erro ao fazer upload da imagem. Detalhes: ' + error.message));
+          return reject(new HttpException('Erro ao fazer upload da imagem. Detalhes: ' + error.message, HttpStatus.BAD_REQUEST));
         }
         resolve(result as UploadApiResponse);
       });
