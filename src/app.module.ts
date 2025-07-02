2a9afba88e7b46ac46773ac1bdbbdf9845aac0fe
Gabriel da Silva Lomenha <gabriellomenha@gmail.com>
2025-07-02 01:16:36 -0300

    criando arquivos iniciais da entidade duv

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { NavioModule } from './navio/navio.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
 import { PessoaModule } from './pessoa/pessoa.module';
+import { DuvModule } from './duv/duv.module';
 
 @Module({
-  imports: [PrismaModule, CloudinaryModule, NavioModule, PessoaModule],
+  imports: [PrismaModule, CloudinaryModule, NavioModule, PessoaModule, DuvModule],
   controllers: [],
   providers: [],
 })
