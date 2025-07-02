eb8e08a7b0e79ce2031a56e42f6ceb3834a80124
Gabriel da Silva Lomenha <gabriellomenha@gmail.com>
2025-07-02 00:54:57 -0300

    feat: implementa criação de tripulantes e passageiros com upload de imagem

--- src/pessoa/dto/create-pessoa.dto.ts
+++ src/pessoa/dto/create-pessoa.dto.ts
@@ -1,1 +1,11 @@
-export class CreatePessoaDto {}
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreatePessoaDto {
+    @IsNotEmpty()
+    @IsString()
+    NO_pessoa: string;
+
+    @IsNotEmpty()
+    @IsString()
+    DS_nacionalidade: string;
+}
