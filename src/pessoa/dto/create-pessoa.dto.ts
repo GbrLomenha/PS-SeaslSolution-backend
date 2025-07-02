d71dac3ed5081a4388b1d14a7e856091247e03e7
Gabriel da Silva Lomenha <gabriellomenha@gmail.com>
2025-07-02 02:14:07 -0300

    docs: documentando entradas de DTO

--- src/pessoa/dto/create-pessoa.dto.ts
+++ src/pessoa/dto/create-pessoa.dto.ts
@@ -1,10 +1,23 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString } from "class-validator";
 
 export class CreatePessoaDto {
+    @ApiProperty({
+        description: 'Nome completo da pessoa',
+        example: 'João da Silva',
+        type: String,
+        required: true,
+    })
     @IsNotEmpty()
     @IsString()
     NO_pessoa: string;
 
+    @ApiProperty({
+        description: 'Nacionalidade da pessoa',
+        example: 'Brasileira',
+        type: String,
+        required: true,
+    })
     @IsNotEmpty()
     @IsString()
     DS_nacionalidade: string;
